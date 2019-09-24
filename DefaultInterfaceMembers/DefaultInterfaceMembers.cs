aebfebbbd6bf892fb1fc18a0be2fe6ce510d072a
accedia <maria.doneva@CMP-00084>
2019-09-24 21:56:06 +0300

    Added DefaultInterfaceMembers examples

---
+++ DefaultInterfaceMembers/DefaultInterfaceMembers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DefaultInterfaceMembers
+{
+    class DefaultInterfaceMembers
+    {
+        public interface IFigure
+        {
+            public string Print() => $"This is IFigure: {this.ToString()}";
+
+        }
+
+        public interface IRectangle : IFigure
+        {
+            public new string Print() => $"This is IRectangle: {this.ToString()}";
+        }
+
+        public interface ISquare : IFigure
+        {
+            public new string Print() => $"This is ISquare: {this.ToString()}";
+        }
+
+        public interface ITetragon : IRectangle, ISquare { }
+
+        public class Tetragon : ITetragon { }
+
+        static void Main(string[] args)
+        {
+            ITetragon tetragon = new Tetragon();
+            // C# 8.0 will solve this issue by introducing the most specific override rule. 
+            // It means that in our case illustrated above, the compiler will issue an error and won’t let us do this.
+            //Console.WriteLine(value: $"{tetragon.Print()}");
+        }
+    }
+}
---
+++ DefaultInterfaceMembersStandardCase/DefaultInterfaceMembersStandardCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DefaultInterfaceMembersStandardCase
+{
+    class DefaultInterfaceMembersStandardCase
+    {
+        public interface IFigure
+        {
+            public double Area();
+            //public string Print() => $"Print() of IFigure: {this.ToString()} at {DateTime.Now}";
+        }
+
+        public class Rectangle : IFigure
+        {
+            public double SideA { get; set; }
+            public double SideB { get; set; }
+
+            public double Area() => SideA * SideB;
+        }
+
+        public class Square : IFigure
+        {
+            public double SideA { get; set; }
+
+            public double Area() => SideA * SideA;
+        }
+
+        static void Main(string[] args)
+        {
+            IFigure rectangle = new Rectangle { SideA = 4, SideB = 5 };
+            Console.WriteLine($"Area of rectangle is {rectangle.Area()}");
+            //Console.WriteLine($"{rectangle.Print()}");
+
+            IFigure square = new Square { SideA = 4};
+            Console.WriteLine($"Area of square is {square.Area()}");
+            //Console.WriteLine($"{square.Print()}");
+        }
+    }
+}
