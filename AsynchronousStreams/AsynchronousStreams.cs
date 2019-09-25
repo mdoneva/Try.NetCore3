75ea31e69bfb3fed573c565b94fa019c137341ba
accedia <maria.doneva@CMP-00084>
2019-09-25 17:51:40 +0300

    Added examples for Asynchronous streams

---
+++ AsynchronousStreams/AsynchronousStreams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsynchronousStreams
+{
+    class AsynchronousStreams
+    {
+        //static async Task Main(string[] args)
+        //{
+        //    string[] names = new string[] { "John", "Jack", "Ana", "Steven", "Jane", "Peter", "Chris" };
+
+        //    IAsyncEnumerable<Customer> customers = CustomerAPI.GetCustomersAsyncStream(names[0..]);
+
+        //    // Consuming an asynchronous stream requires you to add the 
+        //    // await keyword before the foreach keyword
+        //    await foreach (var customer in customers)
+        //    {
+        //        Console.WriteLine(customer);
+        //    }
+        //}
+
+        static async Task Main(string[] args)
+        {
+            string[] names = new string[] { "John", "Jack", "Ana", "Steven", "Jane", "Peter", "Chris" };
+
+            IEnumerable<Customer> customers = await CustomerAPI.GetCustomersAsync(names);
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+        }
+    }
+}
---
+++ AsynchronousStreams/CustomerAPI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousStreams
+{
+    public class Customer
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public override string ToString() => $"Customer {Name} is {Age} years old.";
+    }
+
+    public static class CustomerAPI
+    {
+        public static async IAsyncEnumerable<Customer> GetCustomersAsyncStream(string[] names)
+        {
+            foreach (var name in names)
+            {
+                // Simulate waiting for data to come through the API
+                await Task.Delay(1000);
+                yield return new Customer { Age = GetAge(), Name = name };
+            }
+        }
+
+        // Common intuitive approach, it's a trap
+        public static async Task<IEnumerable<Customer>> GetCustomersAsync(string[] names)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            foreach (var name in names)
+            {
+                // Simulate waiting for data to come through. 
+                await Task.Delay(1000);
+                customers.Add(new Customer { Age = GetAge(), Name = name });
+            }
+
+            return customers;
+        }
+
+        private static int GetAge()
+        {
+            Random random = new Random();
+            return random.Next(20, 40);
+        }
+    }
+}
